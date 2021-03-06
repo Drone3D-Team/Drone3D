4f8c7cf4e4ef62f67427fb7142480ecbf3e2a964
Gabriel Fleischer <gabriel.fleischer@epfl.ch>
2021-03-06 20:21:49 +0100

    100 Coverage

--- app/src/androidTest/java/ch/epfl/sdp/drone3d/MainActivityTest.kt
+++ app/src/androidTest/java/ch/epfl/sdp/drone3d/MainActivityTest.kt
@@ -1,16 +1,22 @@
 package ch.epfl.sdp.drone3d
 
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
 import org.junit.Assert.assertEquals
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+
 
 /**
  * Test for the main activity
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+
+    @get:Rule
+    var testRule = ActivityScenarioRule(MainActivity::class.java)
 
     /**
      * Make sure the context of the app is the right one
