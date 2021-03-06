044e81ef5ca12bfb9b80f83eb0fee1acb8b6a5f7
Gabriel Fleischer <gabriel.fleischer@epfl.ch>
2021-03-06 19:54:52 +0100

    Add a basic test

---
+++ app/src/androidTest/java/ch/epfl/sdp/drone3d/MainActivityTest.kt
@@ -0,0 +1,24 @@
+package ch.epfl.sdp.drone3d
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import org.junit.runner.RunWith
+
+/**
+ * Test for the main activity
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+
+    /**
+     * Make sure the context of the app is the right one
+     */
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("ch.epfl.sdp.drone3d", appContext.packageName)
+    }
+}
