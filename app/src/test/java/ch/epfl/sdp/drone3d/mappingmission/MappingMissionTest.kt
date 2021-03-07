f520457d9c173740147cd3ecb4d259e174e748f3
Squalene <anamaste@mail.com>
2021-03-07 22:12:00 +0100

    Add tested serializable mapping mission

--- app/src/test/java/ch/epfl/sdp/drone3d/mappingmission/MappingMissionTest.kt
+++ app/src/test/java/ch/epfl/sdp/drone3d/mappingmission/MappingMissionTest.kt
@@ -1,2 +1,34 @@
 package ch.epfl.sdp.drone3d.mappingmission
 
+import com.google.android.gms.maps.model.LatLng
+import junit.framework.TestCase.assertNull
+import org.hamcrest.CoreMatchers.equalTo
+import org.hamcrest.MatcherAssert
+import org.junit.Test
+
+
+class MappingMissionTest {
+    @Test
+    fun serializationAndDeserializationForTwoCoordinatesMissionIsCorrect() {
+        val mappingMission = MappingMission(listOf(LatLng(0.0, 0.0), LatLng(1.0, 1.0)))
+        MatcherAssert.assertThat(
+            MappingMission.deserialize(mappingMission.serialized()),
+            equalTo(mappingMission)
+        )
+    }
+
+    @Test
+    fun serializationAndDeserializationForEmptyMissionIsCorrect() {
+        val mappingMission = MappingMission(emptyList())
+        MatcherAssert.assertThat(
+            MappingMission.deserialize(mappingMission.serialized()),
+            equalTo(mappingMission)
+        )
+    }
+
+    @Test
+    fun deserializationForIncorrectStringReturnsNull() {
+        val serialized = "{ { }"
+        assertNull(MappingMission.deserialize(serialized))
+    }
+}
