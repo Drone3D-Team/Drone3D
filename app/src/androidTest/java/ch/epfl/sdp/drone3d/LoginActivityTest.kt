4d7a6eb40bc7f911c44e07c5e708e68972974f17
Gabriel Fleischer <gabriel.fleischer@epfl.ch>
2021-03-10 17:18:08 +0100

    Cleaning the code, fixing tests and removing unused dependencies

--- app/src/androidTest/java/ch/epfl/sdp/drone3d/LoginActivityTest.kt
+++ app/src/androidTest/java/ch/epfl/sdp/drone3d/LoginActivityTest.kt
@@ -3,14 +3,13 @@ package ch.epfl.sdp.drone3d
 import androidx.test.espresso.Espresso
 import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.intent.Intents
 import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.withEffectiveVisibility
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
-import org.junit.Assert
-import org.junit.Rule
-import org.junit.Test
+import org.junit.*
 import org.junit.runner.RunWith
 
 /**
@@ -21,6 +20,16 @@ class LoginActivityTest {
 
     @get:Rule
     var testRule = ActivityScenarioRule(LoginActivity::class.java)
+
+    @Before
+    fun setUp() {
+        Intents.init()
+    }
+
+    @After
+    fun tearDown() {
+        Intents.release()
+    }
 
     /**
      * Make sure the context of the app is the right one
