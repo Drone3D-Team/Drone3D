0ce05a4d71ef23a314ef5d645bc5c56a984d6f4e
Gabriel Fleischer <gabriel.fleischer@epfl.ch>
2021-03-12 01:01:26 +0100

    Test authentication service

--- app/src/main/java/ch/epfl/sdp/drone3d/auth/AuthenticationServiceImpl.kt
+++ app/src/main/java/ch/epfl/sdp/drone3d/auth/AuthenticationServiceImpl.kt
@@ -3,18 +3,11 @@ package ch.epfl.sdp.drone3d.auth
 import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.ktx.Firebase
 
-class AuthenticationServiceImpl : AuthenticationService{
-
-    private lateinit var auth: FirebaseAuth
-
-    private fun getAuth(): FirebaseAuth {
-        if (!this::auth.isInitialized)
-            auth = Firebase.auth
-        return auth
-    }
+/**
+ * Implementation of the authentication service based on firebase
+ */
+class AuthenticationServiceImpl(private val auth: FirebaseAuth) : AuthenticationService {
 
     override fun hasActiveSession(): Boolean {
         return auth.currentUser != null
@@ -26,18 +19,18 @@ class AuthenticationServiceImpl : AuthenticationService{
     }
 
     override fun register(email: String, password: String): Task<AuthResult> {
-        return getAuth().createUserWithEmailAndPassword(email, password)
+        return auth.createUserWithEmailAndPassword(email, password)
     }
 
     override fun login(email: String, password: String): Task<AuthResult> {
-        return getAuth().signInWithEmailAndPassword(email, password)
+        return auth.signInWithEmailAndPassword(email, password)
     }
 
     override fun sendPasswordReset(email: String): Task<Void> {
-        return getAuth().sendPasswordResetEmail(email)
+        return auth.sendPasswordResetEmail(email)
     }
 
     override fun signOut() {
-        getAuth().signOut()
+        auth.signOut()
     }
 }
