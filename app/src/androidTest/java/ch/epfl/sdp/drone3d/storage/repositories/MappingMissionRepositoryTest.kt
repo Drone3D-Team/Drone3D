ce95c336d9f2789adf9d14ce9afa167ff9837b9e
Squalene <anamaste@mail.com>
2021-03-14 11:47:28 +0100

    Add MappingMissionDao tests and gradle dependencies

--- app/src/androidTest/java/ch/epfl/sdp/drone3d/storage/repositories/MappingMissionRepositoryTest.kt
+++ app/src/androidTest/java/ch/epfl/sdp/drone3d/storage/repositories/MappingMissionRepositoryTest.kt
@@ -7,9 +7,9 @@ import androidx.test.ext.junit.runners.AndroidJUnit4
 import ch.epfl.sdp.drone3d.storage.dao.MappingMissionDao
 import ch.epfl.sdp.drone3d.storage.data.LatLong
 import ch.epfl.sdp.drone3d.storage.data.MappingMission
-import ch.epfl.sdp.drone3d.storage.repositories.MappingMissionRepository
-import org.hamcrest.CoreMatchers
+import org.hamcrest.CoreMatchers.equalTo
 import org.hamcrest.MatcherAssert.assertThat
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -20,28 +20,110 @@ class MappingMissionRepositoryTest {
 
     private val testOwnerUid = "123456"
     private val testPrivateId = "privateId"
-    private val testPublicId = "privateId"
-    private val testMission = MappingMission("Lausanne", listOf(LatLong(0.0,0.0),LatLong(1.0,1.0)))
+    private val testSharedId = "privateId"
+    private val testMission =
+        MappingMission("Lausanne", listOf(LatLong(0.0, 0.0), LatLong(1.0, 1.0)))
+
+    private var dao = Mockito.mock(MappingMissionDao::class.java)
+    private var repo = MappingMissionRepository()
 
     @get:Rule
     val instantTaskExecutorRule = InstantTaskExecutorRule()//Makes all actions synchronous
 
-    @Test
-    fun getPrivateMappingMissionCallsGetPrivateMappingMissionOfDao(){
-        val dao = Mockito.mock(MappingMissionDao::class.java)
-        val expectedData = MutableLiveData<MappingMission>()
-        expectedData.value = testMission
-        Mockito.`when`(dao.getPrivateMappingMission(testOwnerUid,testPrivateId)).thenReturn(expectedData)
-
+    @Before
+    fun beforeTests() {
+        dao = Mockito.mock(MappingMissionDao::class.java)
         MappingMissionRepository.daoProvider = { dao }
-        val repo =
-            MappingMissionRepository()
-
-        assertThat(repo.getPrivateMappingMission(testOwnerUid,testPrivateId),
-            CoreMatchers.equalTo(expectedData as LiveData<MappingMission>)
-        )
-        Mockito.verify(dao, Mockito.times(1)).getPrivateMappingMission(testOwnerUid,testPrivateId)
+        repo = MappingMissionRepository()
     }
 
+    @Test
+    fun getPrivateMappingMissionCallsGetPrivateMappingMissionOfDao() {
 
+        val expectedData = MutableLiveData<MappingMission>()
+        expectedData.value = testMission
+        Mockito.`when`(dao.getPrivateMappingMission(testOwnerUid, testPrivateId))
+            .thenReturn(expectedData)
+
+        assertThat(
+            repo.getPrivateMappingMission(testOwnerUid, testPrivateId),
+            equalTo(expectedData as LiveData<MappingMission>)
+        )
+        Mockito.verify(dao, Mockito.times(1)).getPrivateMappingMission(testOwnerUid, testPrivateId)
+    }
+
+    @Test
+    fun getSharedMappingMissionCallsGetSharedMappingMissionOfDao() {
+
+        val expectedData = MutableLiveData<MappingMission>()
+        expectedData.value = testMission
+        Mockito.`when`(dao.getSharedMappingMission(testOwnerUid, testSharedId))
+            .thenReturn(expectedData)
+
+        assertThat(
+            repo.getSharedMappingMission(testOwnerUid, testSharedId),
+            equalTo(expectedData as LiveData<MappingMission>)
+        )
+        Mockito.verify(dao, Mockito.times(1)).getSharedMappingMission(testOwnerUid, testSharedId)
+    }
+
+    @Test
+    fun getPrivateMappingMissionsCallsGetPrivateMappingMissionsOfDao() {
+
+        val expectedData = MutableLiveData<List<MappingMission>>()
+        expectedData.value = listOf(testMission)
+        Mockito.`when`(dao.getPrivateMappingMissions(testOwnerUid)).thenReturn(expectedData)
+
+        assertThat(
+            repo.getPrivateMappingMissions(testOwnerUid),
+            equalTo(expectedData as LiveData<List<MappingMission>>)
+        )
+        Mockito.verify(dao, Mockito.times(1)).getPrivateMappingMissions(testOwnerUid)
+    }
+
+    @Test
+    fun getSharedMappingMissionsCallsGetSharedMappingMissionsOfDao() {
+
+        val expectedData = MutableLiveData<List<MappingMission>>()
+        expectedData.value = listOf(testMission)
+        Mockito.`when`(dao.getSharedMappingMissions()).thenReturn(expectedData)
+
+        assertThat(
+            repo.getSharedMappingMissions(),
+            equalTo(expectedData as LiveData<List<MappingMission>>)
+        )
+        Mockito.verify(dao, Mockito.times(1)).getSharedMappingMissions()
+    }
+
+    @Test
+    fun storeMappingMissionsCallsStoreMappingMissionsOfDao() {
+        repo.storeMappingMission(testOwnerUid, testMission)
+        Mockito.verify(dao, Mockito.times(1)).storeMappingMission(testOwnerUid, testMission)
+    }
+
+    @Test
+    fun shareMappingMissionsCallsShareMappingMissionsOfDao() {
+        repo.shareMappingMission(testOwnerUid, testMission)
+        Mockito.verify(dao, Mockito.times(1)).shareMappingMission(testOwnerUid, testMission)
+    }
+
+    @Test
+    fun removePrivateMappingMissionsCallsRemovePrivateMappingMissionsOfDao() {
+        repo.removePrivateMappingMission(testOwnerUid, testPrivateId)
+        Mockito.verify(dao, Mockito.times(1))
+            .removePrivateMappingMission(testOwnerUid, testPrivateId)
+    }
+
+    @Test
+    fun removeSharedMappingMissionsCallsRemoveSharedMappingMissionsOfDao() {
+        repo.removeSharedMappingMission(testOwnerUid, testSharedId)
+        Mockito.verify(dao, Mockito.times(1)).removeSharedMappingMission(testOwnerUid, testSharedId)
+    }
+
+    @Test
+    fun removeMappingMissionsCallsRemovedMappingMissionsOfDao() {
+        repo.removeMappingMission(testOwnerUid, testPrivateId, testSharedId)
+        Mockito.verify(dao, Mockito.times(1))
+            .removeMappingMission(testOwnerUid, testPrivateId, testSharedId)
+    }
 }
