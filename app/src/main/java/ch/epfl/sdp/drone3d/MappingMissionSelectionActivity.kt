0f4d03b4dcd52548cba9eaabdaf47fe67017cbe8
Jose <costa.coelho.jose@gmail.com>
2021-03-18 19:28:45 +0100

    Add distinction between private and shared mapping missions

--- app/src/main/java/ch/epfl/sdp/drone3d/MappingMissionSelectionActivity.kt
+++ app/src/main/java/ch/epfl/sdp/drone3d/MappingMissionSelectionActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.LinearLayout
+import android.widget.ToggleButton
 import ch.epfl.sdp.drone3d.data.MappingMission
 
 /**
@@ -11,33 +12,38 @@ import ch.epfl.sdp.drone3d.data.MappingMission
  */
 class MappingMissionSelectionActivity : AppCompatActivity() {
 
+    private lateinit var selectedStorageTypeToggleButton: ToggleButton
     private lateinit var mappingMissionListView: LinearLayout
     private lateinit var createNewMappingMissionButton: Button
     private val buttonList = mutableListOf<Button>()
-    private val mappingMissionList = mutableListOf<MappingMission>()
+    private val mappingMissionPrivateList = mutableListOf<MappingMission>()
+    private val mappingMissionSharedList = mutableListOf<MappingMission>()
+
+    enum class StorageType(val checked: Boolean) {
+        PRIVATE(true), SHARED(false)
+    }
+
+    private var currentType = StorageType.PRIVATE
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mapping_mission_selection)
 
+        selectedStorageTypeToggleButton = findViewById(R.id.toggleButton)
         mappingMissionListView = findViewById(R.id.mappingMissionList)
         createNewMappingMissionButton = findViewById(R.id.createMappingMissionButton)
+
+        selectedStorageTypeToggleButton.isChecked = currentType.checked
+        selectedStorageTypeToggleButton.setOnCheckedChangeListener { buttonView, isChecked ->
+            currentType = if (isChecked) StorageType.PRIVATE else StorageType.SHARED
+            updateList()
+        }
 
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         populateMappingMissionsList()
 
-        for (mission in mappingMissionList) {
-            val button = Button(this)
-            button.height = 50
-            button.width = 100
-            button.text = mission.name
-            button.setOnClickListener {
-                //TODO: go to mission details with edit and mission launch options
-            }
-            buttonList.add(button)
-            mappingMissionListView.addView(button)
-        }
+        updateList()
 
         createNewMappingMissionButton.setOnClickListener {
             //TODO: go to mapping mission creation
@@ -47,7 +53,33 @@ class MappingMissionSelectionActivity : AppCompatActivity() {
 
     private fun populateMappingMissionsList() {
         for (i in 0..10) {
-            mappingMissionList.add(MappingMission(emptyList(), "Mission $i"))
+            mappingMissionPrivateList.add(MappingMission(emptyList(), "Mission $i"))
+        }
+        for (i in 10..20) {
+            mappingMissionSharedList.add(MappingMission(emptyList(), "Mission $i"))
+        }
+    }
+
+    private fun updateList() {
+        mappingMissionListView.removeAllViews()
+        buttonList.clear()
+
+        val list: MutableList<MappingMission>
+        when (currentType) {
+            StorageType.PRIVATE -> list = mappingMissionPrivateList
+            StorageType.SHARED -> list = mappingMissionSharedList
+        }
+
+        for (mission in list) {
+            val button = Button(this)
+            button.height = 50
+            button.width = 100
+            button.text = mission.name
+            button.setOnClickListener {
+                //TODO: go to mission details
+            }
+            buttonList.add(button)
+            mappingMissionListView.addView(button)
         }
     }
 }
