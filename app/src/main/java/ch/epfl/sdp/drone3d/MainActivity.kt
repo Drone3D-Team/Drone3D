29f0b073ff69a3fad707f4bf4ee91f56a2da41de
Gabriel Fleischer <gabriel.fleischer@epfl.ch>
2021-03-24 17:35:57 +0100

    Add logout button to main activity

--- app/src/main/java/ch/epfl/sdp/drone3d/MainActivity.kt
+++ app/src/main/java/ch/epfl/sdp/drone3d/MainActivity.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import ch.epfl.sdp.drone3d.auth.AuthenticationService
 import dagger.hilt.android.AndroidEntryPoint
@@ -22,6 +23,26 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_menu)
+
+        refresh()
+    }
+
+    fun refresh() {
+        val loginButton: Button = findViewById(R.id.log_in_button)
+        val logoutButton: Button = findViewById(R.id.log_out_button)
+
+        if (authService.hasActiveSession()) {
+            logoutButton.visibility = View.VISIBLE
+            loginButton.visibility = View.GONE
+        } else {
+            logoutButton.visibility = View.GONE
+            loginButton.visibility = View.VISIBLE
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        refresh()
     }
 
     /**
@@ -29,6 +50,14 @@ class MainActivity : AppCompatActivity() {
      */
     fun goToLogin(@Suppress("UNUSED_PARAMETER") view: View) {
         open(LoginActivity::class)
+    }
+
+    /**
+     * Go to LoginActivity when log_in_button is clicked
+     */
+    fun logout(@Suppress("UNUSED_PARAMETER") view: View) {
+        authService.signOut()
+        refresh()
     }
 
     /**
