59bd8c2ed4e5272d76ba2c18a588ea1915378232
NicolasGilliard <nicolas.gilliard@epfl.ch>
2021-04-19 17:44:42 +0200

    Connecting a simulation refuses invalid ip and port

--- app/src/androidTest/java/ch/epfl/sdp/drone3d/ui/drone/DroneConnectActivityTest.kt
+++ app/src/androidTest/java/ch/epfl/sdp/drone3d/ui/drone/DroneConnectActivityTest.kt
@@ -12,12 +12,13 @@ import androidx.test.espresso.intent.matcher.ComponentNameMatchers.hasClassName
 import androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent
 import androidx.test.espresso.matcher.ViewMatchers.isRoot
 import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.rule.ActivityTestRule
 import ch.epfl.sdp.drone3d.R
 import ch.epfl.sdp.drone3d.drone.DroneInstanceMock
 import ch.epfl.sdp.drone3d.drone.DroneModule
 import ch.epfl.sdp.drone3d.drone.DroneService
+import ch.epfl.sdp.drone3d.matcher.ToastMatcher
 import dagger.hilt.android.testing.BindValue
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
@@ -34,9 +35,11 @@ import org.mockito.Mockito.*
 @UninstallModules(DroneModule::class)
 class DroneConnectActivityTest {
 
+    private val activityRule = ActivityTestRule(DroneConnectActivity::class.java)
+
     @get:Rule
-    val activityRule: RuleChain = RuleChain.outerRule(HiltAndroidRule(this))
-            .around(ActivityScenarioRule(DroneConnectActivity::class.java))
+    val testRule: RuleChain = RuleChain.outerRule(HiltAndroidRule(this))
+            .around(activityRule)
 
     @BindValue
     val droneService: DroneService = DroneInstanceMock.mockService()
@@ -87,6 +90,14 @@ class DroneConnectActivityTest {
         Intents.intended(
             hasComponent(hasClassName(ConnectedDroneActivity::class.java.name))
         )
+    }
+
+    @Test
+    fun connectSimulatedDroneShowToastWhenInvalidIp() {
+        onView(withId(R.id.connect_simulation_button))
+            .perform(click())
+
+        ToastMatcher.onToast(activityRule.activity, R.string.ip_format_invalid)
     }
 
     @Test
--- app/src/main/java/ch/epfl/sdp/drone3d/ui/drone/DroneConnectActivity.kt
+++ app/src/main/java/ch/epfl/sdp/drone3d/ui/drone/DroneConnectActivity.kt
@@ -12,11 +12,23 @@ import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import ch.epfl.sdp.drone3d.R
 import ch.epfl.sdp.drone3d.drone.DroneService
+import ch.epfl.sdp.drone3d.ui.ToastHandler
 import dagger.hilt.android.AndroidEntryPoint
+import java.util.regex.Matcher
+import java.util.regex.Pattern
 import javax.inject.Inject
 
 @AndroidEntryPoint
 class DroneConnectActivity : AppCompatActivity() {
+
+    /**
+     * Values used to check if an ip has a valid format or not
+     */
+    private val regex = "^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\." +
+            "(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\." +
+            "(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\." +
+            "(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$"
+    private val pattern: Pattern = Pattern.compile(regex)
 
     @Inject
     lateinit var droneService: DroneService
@@ -31,10 +43,14 @@ class DroneConnectActivity : AppCompatActivity() {
      */
     fun connectSimulatedDrone(@Suppress("UNUSED_PARAMETER") view: View) {
         val ip = findViewById<EditText>(R.id.text_IP_address).text.toString()
-        val port = findViewById<EditText>(R.id.text_port).text.toString()
-        droneService.setSimulation(ip, port)
-        val intent = Intent(this, ConnectedDroneActivity::class.java)
-        startActivity(intent)
+        if (verifyIp(ip)) {
+            val port = findViewById<EditText>(R.id.text_port).text.toString()
+            droneService.setSimulation(ip, port)
+            val intent = Intent(this, ConnectedDroneActivity::class.java)
+            startActivity(intent)
+        } else {
+            ToastHandler.showToastAsync(this, R.string.ip_format_invalid)
+        }
     }
 
     /**
@@ -44,5 +60,13 @@ class DroneConnectActivity : AppCompatActivity() {
         droneService.setDrone()
         val intent = Intent(this, ConnectedDroneActivity::class.java)
         startActivity(intent)
+    }
+
+    /**
+     * Checks if an ip has a valid format or not
+     */
+    private fun verifyIp(ip: String): Boolean {
+        val matcher: Matcher = pattern.matcher(ip)
+        return matcher.matches()
     }
 }
