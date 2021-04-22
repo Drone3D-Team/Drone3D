2c832cb554e2442b4bb773110cf423874c85e715
Jose <costa.coelho.jose@gmail.com>
2021-04-22 12:29:48 +0200

    Add comment for clarification of getFilteredMappingMission

--- app/src/main/java/ch/epfl/sdp/drone3d/service/storage/dao/MappingMissionDao.kt
+++ app/src/main/java/ch/epfl/sdp/drone3d/service/storage/dao/MappingMissionDao.kt
@@ -35,15 +35,17 @@ interface MappingMissionDao {
     fun getSharedMappingMissions(): LiveData<List<MappingMission>>
 
     /**
-     * Returns the list of all private mapping missions filtered by the current filter,
-     * this live data is updated in continuous time if the private missions are modified
+     * Returns the list of all private mapping missions filtered by the current filter.
+     * When initially called, the return list is empty until updatePrivateFilteredMappingMissions is called.
+     * This live data is updated in continuous time if the private missions are modified
      * and also when updatePrivateFilteredMappingMissions is called with another filter
      */
     fun getPrivateFilteredMappingMissions(): LiveData<List<MappingMission>>
 
     /**
-     * Returns the list of all shared mapping missions filtered by the current filter,
-     * this live data is updated in continuous time if the shared missions are modified
+     * Returns the list of all shared mapping missions filtered by the current filter.
+     * When initially called, the return list is empty until updateSharedFilteredMappingMissions is called.
+     * This live data is updated in continuous time if the shared missions are modified
      * and also when updateSharedFilteredMappingMissions is called with another filter
      */
     fun getSharedFilteredMappingMissions(): LiveData<List<MappingMission>>
