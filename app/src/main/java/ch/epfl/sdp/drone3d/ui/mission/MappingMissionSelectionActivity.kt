17367c3e5207cc5d431dd9104ba777ffda7f3942
José <44306955+Jorque@users.noreply.github.com>
2021-04-22 19:26:42 +0200

    Search bar for mapping missions (#154)
    * Implement search of mapping missions but query bug
    
    * Fix query bug and set search bar behaviour
    
    * Test search functionality
    
    * Fix main activity test
    
    * Refactor onCreate of MappingMissionSelectionActivity
    
    * Refractor setupListViews
    
    * Refractor again setupListViews
    
    * Rollback change on test
    
    * Fix test bug and enable search non case sensitive
    
    * Remove warnings
    
    * Code climate complaining again...
    
    * Add comment for clarification of getFilteredMappingMission
    
    * Add tests for getter and updater of mapping missions
    
    * Show mission in right order

--- app/src/main/java/ch/epfl/sdp/drone3d/ui/mission/MappingMissionSelectionActivity.kt
+++ app/src/main/java/ch/epfl/sdp/drone3d/ui/mission/MappingMissionSelectionActivity.kt
@@ -10,6 +10,8 @@ import android.os.Bundle
 import android.view.View
 import android.view.View.GONE
 import android.view.View.VISIBLE
+import android.widget.ImageView
+import android.widget.SearchView
 import android.widget.ToggleButton
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.LiveData
@@ -21,6 +23,7 @@ import ch.epfl.sdp.drone3d.service.auth.AuthenticationService
 import ch.epfl.sdp.drone3d.service.storage.dao.MappingMissionDao
 import ch.epfl.sdp.drone3d.service.storage.data.MappingMission
 import dagger.hilt.android.AndroidEntryPoint
+import java.util.*
 import javax.inject.Inject
 
 
@@ -36,55 +39,180 @@ class MappingMissionSelectionActivity : AppCompatActivity() {
     @Inject
     lateinit var authService: AuthenticationService
 
-    enum class StorageType(val checked: Boolean, val sharedVisible: Boolean, val privateVisible: Boolean) {
+    enum class StorageType(
+        val checked: Boolean,
+        val sharedVisible: Boolean,
+        val privateVisible: Boolean
+    ) {
         PRIVATE(true, false, true),
         SHARED(false, true, false)
     }
 
-    private val currentType = MutableLiveData(StorageType.PRIVATE)
+    private val currentListState =
+        MutableLiveData(Pair<StorageType, String?>(StorageType.PRIVATE, null))
 
-    private fun setupAdapter(data: LiveData<List<MappingMission>>,
-                             adapter: ListAdapter<MappingMission, out RecyclerView.ViewHolder>) =
-            data.observe(this) {
-                it?.let { adapter.submitList(it) }
+    private fun setupAdapter(
+        data: LiveData<List<MappingMission>>,
+        adapter: ListAdapter<MappingMission, out RecyclerView.ViewHolder>
+    ) = data.observe(this) {
+        it.let { adapter.submitList(sortedList(it)) }
+    }
+
+
+    private fun sortedList(mappingMissions: List<MappingMission>?): List<MappingMission> {
+        if (mappingMissions != null && mappingMissions.isNotEmpty()) {
+            val list = mappingMissions.toList()
+            Collections.sort(
+                list
+            ) { o1, o2 -> compare(o1, o2) }
+            return list
+        }
+        return emptyList()
+    }
+
+    private fun compare(m1: MappingMission, m2: MappingMission): Int {
+        val name1 = m1.nameUpperCase
+        val name2 = m2.nameUpperCase
+        val smallerStringSize = if (name1.length < name2.length) name1.length else name2.length
+        val diffIndex = indexOfDifference(name1, name2)
+
+        if (diffIndex < smallerStringSize) {
+            return if (name1[diffIndex].isDigit() && name2[diffIndex].isDigit()) {
+                extractInt(name1.substring(diffIndex)) - extractInt(name2.substring(diffIndex))
+            } else if (name1[diffIndex].isDigit()) {
+                1
+            } else if (name2[diffIndex].isDigit()) {
+                -1
+            } else {
+                name1.compareTo(name2)
             }
+        }
+
+        return name1.compareTo(name2)
+    }
+
+    private fun extractInt(s: String): Int {
+        val num = s.replace("\\D".toRegex(), "")
+        return if (num.isEmpty()) 0 else num.toInt()
+    }
+
+    private fun indexOfDifference(s1: String, s2: String): Int {
+        if (s1.isNotEmpty() && s2.isNotEmpty()) {
+            val smaller = if (s1.length < s2.length) s1.length else s2.length
+            for (i in 0 until smaller) {
+                if (s1[i] != s2[i]) return i
+            }
+            return smaller
+        }
+        return 0
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mapping_mission_selection)
 
-        val ownerId = authService.getCurrentSession()!!.user.uid
+        setupListViews()
 
-        val sharedAdapter = MissionViewAdapter( false)
-        val privateAdapter = MissionViewAdapter(true)
+        // Setup toggle button
+        val selectedStorageTypeToggleButton =
+            findViewById<ToggleButton>(R.id.mapping_mission_state_toggle)
+        selectedStorageTypeToggleButton.isChecked = currentListState.value!!.first.checked
+        selectedStorageTypeToggleButton.setOnCheckedChangeListener { _, isChecked ->
+            currentListState.value =
+                Pair(
+                    if (isChecked) StorageType.PRIVATE else StorageType.SHARED,
+                    currentListState.value!!.second
+                )
+        }
+
+        val searchBar = findViewById<SearchView>(R.id.searchView)
+        setupSearchBar(searchBar)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    private fun setupListViews() {
+        val ownerId = authService.getCurrentSession()!!.user.uid
 
         val sharedList = findViewById<RecyclerView>(R.id.shared_mission_list_view)
         val privateList = findViewById<RecyclerView>(R.id.private_mission_list_view)
+        val sharedFilteredList = findViewById<RecyclerView>(R.id.shared_filtered_mission_list_view)
+        val privateFilteredList =
+            findViewById<RecyclerView>(R.id.private_filtered_mission_list_view)
 
-        // Setup lists
-        sharedList.adapter = sharedAdapter
-        privateList.adapter = privateAdapter
-
-        // Setup adapters
-        setupAdapter(mappingMissionDao.getSharedMappingMissions(), sharedAdapter)
-        setupAdapter(mappingMissionDao.getPrivateMappingMissions(ownerId), privateAdapter)
+        setupListAdapter(sharedList, false, false)
+        setupListAdapter(privateList, true, false)
+        setupListAdapter(sharedFilteredList, false, true)
+        setupListAdapter(privateFilteredList, true, true)
 
         // Link state with view visibility
-        currentType.observe(this) {
-            it?.let {
-                sharedList.visibility = if (it.sharedVisible) VISIBLE else GONE
-                privateList.visibility = if (it.privateVisible) VISIBLE else GONE
+        currentListState.observe(this) {
+            it.let {
+                sharedList.visibility = getVisibility(false, false, it)
+                privateList.visibility = getVisibility(true, false, it)
+                sharedFilteredList.visibility = getVisibility(false, true, it)
+                privateFilteredList.visibility = getVisibility(true, true, it)
+
+                mappingMissionDao.updateSharedFilteredMappingMissions(it.second)
+                mappingMissionDao.updatePrivateFilteredMappingMissions(ownerId, it.second)
             }
         }
+    }
 
-        // Setup toggle button
-        val selectedStorageTypeToggleButton = findViewById<ToggleButton>(R.id.mapping_mission_state_toggle)
-        selectedStorageTypeToggleButton.isChecked = currentType.value!!.checked
-        selectedStorageTypeToggleButton.setOnCheckedChangeListener { _, isChecked ->
-            currentType.value = if (isChecked) StorageType.PRIVATE else StorageType.SHARED
+    private fun getVisibility(
+        private: Boolean,
+        filter: Boolean,
+        current: Pair<StorageType, String?>
+    ): Int {
+        return when {
+            private && !current.first.privateVisible -> GONE
+            !private && !current.first.sharedVisible -> GONE
+            filter && current.second == null -> GONE
+            !filter && current.second != null -> GONE
+            else -> VISIBLE
         }
+    }
 
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    private fun setupListAdapter(list: RecyclerView, private: Boolean, filter: Boolean) {
+        val adapter = MissionViewAdapter(private)
+        list.adapter = adapter
+        setupAdapter(getMappingMissionsLiveData(private, filter), adapter)
+    }
+
+    private fun getMappingMissionsLiveData(
+        private: Boolean,
+        filter: Boolean
+    ): LiveData<List<MappingMission>> {
+        val ownerId = authService.getCurrentSession()!!.user.uid
+        return when {
+            private and !filter -> mappingMissionDao.getPrivateMappingMissions(ownerId)
+            !private and !filter -> mappingMissionDao.getSharedMappingMissions()
+            private and filter -> mappingMissionDao.getPrivateFilteredMappingMissions()
+            else -> mappingMissionDao.getSharedFilteredMappingMissions()
+        }
+    }
+
+    private fun setupSearchBar(searchBar: SearchView) {
+        // Searches only when submit button is pressed
+        searchBar.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String): Boolean {
+                currentListState.value = Pair(currentListState.value!!.first, query)
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String): Boolean {
+                return false
+            }
+        })
+
+        // When the clear text button is pressed, show all private or shared mapping missions
+        val searchCloseButtonId =
+            searchBar.context.resources.getIdentifier("android:id/search_close_btn", null, null)
+        val closeButton: ImageView = searchBar.findViewById(searchCloseButtonId)
+        closeButton.setOnClickListener {
+            searchBar.setQuery("", false)
+            currentListState.value = Pair(currentListState.value!!.first, null)
+        }
     }
 
     fun createNewMission(@Suppress("UNUSED_PARAMETER") view: View) {
