654123dc9b27bd711b2a99133295ad58cea0ef20
Squalene <anamaste@mail.com>
2021-05-09 10:48:58 +0200

    Add launch download when button is pressed (deactivated for now)

--- app/src/main/java/ch/epfl/sdp/drone3d/ui/map/offline/ManageOfflineMapActivity.kt
+++ app/src/main/java/ch/epfl/sdp/drone3d/ui/map/offline/ManageOfflineMapActivity.kt
@@ -1,21 +1,31 @@
 package ch.epfl.sdp.drone3d.ui.map.offline
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import ch.epfl.sdp.drone3d.R
 import ch.epfl.sdp.drone3d.map.offline.OfflineMapSaver
 import ch.epfl.sdp.drone3d.map.offline.OfflineMapSaverImpl
+import ch.epfl.sdp.drone3d.ui.ToastHandler
 import ch.epfl.sdp.drone3d.ui.map.BaseMapActivity
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.mapbox.mapboxsdk.Mapbox
 import com.mapbox.mapboxsdk.maps.MapboxMap
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback
 import com.mapbox.mapboxsdk.maps.Style
 import com.mapbox.mapboxsdk.offline.OfflineRegion
+import com.mapbox.mapboxsdk.offline.OfflineRegionError
+import com.mapbox.mapboxsdk.offline.OfflineRegionStatus
+import timber.log.Timber
 
 class ManageOfflineMapActivity : BaseMapActivity(), OnMapReadyCallback {
 
     private lateinit var offlineMapSaver: OfflineMapSaver
-
+    private lateinit var mapboxMap: MapboxMap
+    private lateinit var downloadButton: FloatingActionButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,24 +35,56 @@ class ManageOfflineMapActivity : BaseMapActivity(), OnMapReadyCallback {
         mapView.contentDescription = getString(R.string.map_not_ready)
         mapView.getMapAsync(this)
 
+        downloadButton = findViewById(R.id.buttonToSaveOfflineMap)
+        //We must wait for the map to be available to download
+        downloadButton.isEnabled = false
+
+
         //Create a "back button" in the action bar up
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 
     override fun onMapReady(mapboxMap: MapboxMap) {
+        this.mapboxMap = mapboxMap
         // Used to detect when the map is ready in tests
         mapView.contentDescription = getString(R.string.map_ready)
 
         mapboxMap.setStyle(Style.MAPBOX_STREETS) { style ->
 
-            offlineMapSaver = OfflineMapSaverImpl(this, style)
+            offlineMapSaver = OfflineMapSaverImpl(this@ManageOfflineMapActivity, style)
             bindOfflineRegionsToRecycler()
+            downloadButton.isEnabled = true
 
         }
 
 
     }
 
+    fun downloadOfflineMap(@Suppress("UNUSED_PARAMETER") view:View){
+        val bounds = mapboxMap.projection.visibleRegion.latLngBounds
+        val zoom = mapboxMap.cameraPosition.zoom
+        val regionName = "Temp"
+        ToastHandler.showToast(applicationContext,"Hello")
+        Timber.d("Start download")
+//        offlineMapSaver.downloadRegion(regionName,bounds,zoom,object:OfflineRegion.OfflineRegionObserver{
+//            override fun onStatusChanged(status: OfflineRegionStatus) {
+////                val percentage = if (status.requiredResourceCount >= 0) 100.0 * status.completedResourceCount / status.requiredResourceCount else 0.0
+////                Timber.d("Status is $percentage complete")
+//                if(status.isComplete){
+//                    //TODO: check that it is correct
+//                    ToastHandler.showToast(applicationContext, getString(R.string.download_succeeded,regionName))
+//                }
+//            }
+//
+//            override fun onError(error: OfflineRegionError) {
+//                ToastHandler.showToast(applicationContext, R.string.download_failed)
+//            }
+//
+//            override fun mapboxTileCountLimitExceeded(limit: Long) {
+//                ToastHandler.showToast(applicationContext, R.string.tile_limit_exceeded)
+//            }
+//        })
+    }
 
     /**
      * Get the recyclerView, create an adapter and bind it to the offlineRegions by displaying them.
