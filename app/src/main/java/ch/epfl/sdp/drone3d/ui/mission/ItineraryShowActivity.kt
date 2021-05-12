6aec569c876f17e3a29852a43457eb55538f30e1
Gabriel Fleischer <gabriel.fleischer@epfl.ch>
2021-05-12 11:11:44 +0200

    Simplify canMissionBeLaunched expression

--- app/src/main/java/ch/epfl/sdp/drone3d/ui/mission/ItineraryShowActivity.kt
+++ app/src/main/java/ch/epfl/sdp/drone3d/ui/mission/ItineraryShowActivity.kt
@@ -96,15 +96,12 @@ class ItineraryShowActivity : BaseMapActivity() {
      * Check if there is a connected drone, and if the user or the simulation is close enough to launch a mission
      */
     private fun canMissionBeLaunched(): Boolean {
-        return if (currentMissionPath == null || currentMissionPath!!.isEmpty()) {
+        val dronePos = droneService.getData().getPosition().value
+        return if (currentMissionPath?.isEmpty() == true || !droneService.isConnected() || dronePos == null)
             false
-        } else if (!droneService.isConnected()) {
-            false
-        } else {
+        else {
             val beginningPoint = currentMissionPath!![0]
-            val distanceToMission =
-                beginningPoint.distanceTo(droneService.getData().getPosition().value!!)
-            distanceToMission < MAX_BEGINNING_DISTANCE
+            dronePos.distanceTo(beginningPoint)  < MAX_BEGINNING_DISTANCE
         }
     }
 
