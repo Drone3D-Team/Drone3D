8c6259ba6400bbd2a2ac294289e7e108b94642a1
Loïc Houmard <loic.houmard@epfl.ch>
2021-05-17 13:11:48 +0200

    Remove test related to offline manager to avoid concurrency problems

--- app/src/androidTest/java/ch/epfl/sdp/drone3d/ui/map/offline/ManageOfflineMapActivityTest.kt
+++ app/src/androidTest/java/ch/epfl/sdp/drone3d/ui/map/offline/ManageOfflineMapActivityTest.kt
@@ -52,27 +52,6 @@ class ManageOfflineMapActivityTest {
         Intents.init()
     }
 
-    /**
-     * This method clears the database that stores the OfflineRegions in Mapbox before every test.
-     */
-    @Before
-    fun clearOfflineMapDatabaseBefore(){
-        val counter = CountDownLatch(1)
-        val offlineManager = OfflineManager.getInstance(InstrumentationRegistry.getInstrumentation().targetContext)
-        offlineManager.resetDatabase(object : OfflineManager.FileSourceCallback {
-            override fun onSuccess() {
-                counter.countDown()
-            }
-
-            override fun onError(message: String) {
-
-
-            }
-        })
-
-        assert(counter.await(TIMEOUT, TimeUnit.SECONDS))
-    }
-
     @After
     fun tearDown() {
         Intents.release()
@@ -86,16 +65,6 @@ class ManageOfflineMapActivityTest {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         Assert.assertEquals("ch.epfl.sdp.drone3d", appContext.packageName)
-    }
-
-    /**
-     * Make sure the tile count is initialized to 0
-     */
-    @Test
-    fun tileCountIsInitializedTo0() {
-        SystemClock.sleep(2000) //Because the view is a observer and we need some time to make it get the value
-        onView(withId(R.id.tiles_used))
-            .check(matches(withText("0/6000")))
     }
 
     @Test
