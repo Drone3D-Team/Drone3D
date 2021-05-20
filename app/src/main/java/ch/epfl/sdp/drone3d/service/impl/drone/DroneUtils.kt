f09707d7743672a64e61de123ef324bfb75d0402
Squalene <anamaste@mail.com>
2021-05-20 11:29:06 +0200

    Allow a mission item to contain the camera angle

--- app/src/main/java/ch/epfl/sdp/drone3d/service/impl/drone/DroneUtils.kt
+++ app/src/main/java/ch/epfl/sdp/drone3d/service/impl/drone/DroneUtils.kt
@@ -15,13 +15,11 @@ import io.mavsdk.mission.Mission.MissionPlan
 
 object DroneUtils {
 
-    //TODO: replace takePhoto=true by takePhoto parameters provided by a mapping mission: this function
-    // could take a mapping mission
     /**
      * Create a MissionPlan by using a [path] represented by a list of coordinates and the [altitude] wanted
      * for the drone
      */
-    fun makeDroneMission(path: List<LatLng>, altitude: Float, cameraPitch:Float): MissionPlan {
+    fun makeDroneMission(path: List<LatLng>, altitude: Float, cameraPitch:Float?=null): MissionPlan {
         return MissionPlan(path.map { point ->
             generateMissionItem(point.latitude, point.longitude, altitude,cameraPitch,true)
         })
@@ -31,13 +29,13 @@ object DroneUtils {
      * Create a MissionItem by using the coordinates [latitudeDeg] and [longitudeDeg], the [altitude] and the [forwardCameraAngle] wanted
      * for the drone
      */
-    fun generateMissionItem(latitudeDeg: Double, longitudeDeg: Double, altitude: Float,cameraPitch:Float,takePhoto:Boolean): MissionItem {
+    fun generateMissionItem(latitudeDeg: Double, longitudeDeg: Double, altitude: Float,cameraPitch:Float?=null,takePhoto:Boolean = true): MissionItem {
         return MissionItem(
             latitudeDeg,
             longitudeDeg,
             altitude,
             10f,
-            true, cameraPitch, Float.NaN,
+            false, cameraPitch ?: Float.NaN, Float.NaN,
             if(takePhoto)MissionItem.CameraAction.TAKE_PHOTO
                     else MissionItem.CameraAction.NONE,
             Float.NaN, 1.0)
