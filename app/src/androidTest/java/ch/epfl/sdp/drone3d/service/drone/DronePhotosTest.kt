22a4826c367e5e06b8a41f3b8515eaf6e4c17cca
Jose <costa.coelho.jose@gmail.com>
2021-05-24 15:24:25 +0200

    Add photos

--- app/src/androidTest/java/ch/epfl/sdp/drone3d/service/drone/DronePhotosTest.kt
+++ app/src/androidTest/java/ch/epfl/sdp/drone3d/service/drone/DronePhotosTest.kt
@@ -5,27 +5,22 @@
 
 package ch.epfl.sdp.drone3d.service.drone
 
-import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
-import android.util.Log
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.lifecycle.Observer
-import androidx.test.core.app.ApplicationProvider.getApplicationContext
 import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import ch.epfl.sdp.drone3d.R
 import ch.epfl.sdp.drone3d.service.api.drone.DronePhotos
 import ch.epfl.sdp.drone3d.service.api.drone.DroneService
 import ch.epfl.sdp.drone3d.service.impl.drone.DronePhotosImpl
-import dagger.hilt.android.qualifiers.ApplicationContext
 import io.mavsdk.camera.Camera
 import io.mavsdk.camera.CameraProto
 import io.reactivex.BackpressureStrategy
 import io.reactivex.Single
 import io.reactivex.subjects.PublishSubject
 import org.hamcrest.CoreMatchers
-import org.hamcrest.MatcherAssert.*
+import org.hamcrest.MatcherAssert.assertThat
 import org.junit.Assert.assertTrue
 import org.junit.Rule
 import org.junit.Test
@@ -48,10 +43,14 @@ class DronePhotosTest {
         private val DEFAULT_POSITION = Camera.Position(.0, .0, 0f, 0f)
         private val DEFAULT_QUATERNION = Camera.Quaternion(0f, 0f, 0f, 0f)
         private val DEFAULT_EULER_ANGLE = Camera.EulerAngle(0f, 0f, 0f)
-        private const val CORRECT_URL_1 =
-            "https://raw.githubusercontent.com/Drone3D-Team/Drone3D/work-jose-retrieve-photos/app/src/main/res/drawable/photo1.jpeg"
-        private const val CORRECT_URL_2 =
-            "https://raw.githubusercontent.com/Drone3D-Team/Drone3D/work-jose-retrieve-photos/app/src/main/res/drawable/photo2.jpeg"
+        private val CORRECT_URLS = listOf(
+            "https://raw.githubusercontent.com/Drone3D-Team/Drone3D/work-jose-retrieve-photos/app/src/main/res/drawable/photo1.jpeg",
+            "https://raw.githubusercontent.com/Drone3D-Team/Drone3D/work-jose-retrieve-photos/app/src/main/res/drawable/photo2.jpeg",
+            "https://raw.githubusercontent.com/Drone3D-Team/Drone3D/work-jose-retrieve-photos/app/src/main/res/drawable/photo3.jpeg",
+            "https://raw.githubusercontent.com/Drone3D-Team/Drone3D/work-jose-retrieve-photos/app/src/main/res/drawable/photo4.jpeg",
+            "https://raw.githubusercontent.com/Drone3D-Team/Drone3D/work-jose-retrieve-photos/app/src/main/res/drawable/photo5.jpeg",
+            "https://raw.githubusercontent.com/Drone3D-Team/Drone3D/work-jose-retrieve-photos/app/src/main/res/drawable/photo6.jpeg"
+        )
         private const val WRONG_URL = "a wrong url"
     }
 
@@ -85,7 +84,7 @@ class DronePhotosTest {
 
         val counter = CountDownLatch(1)
 
-        val url = URL(CORRECT_URL_1)
+        val url = URL(CORRECT_URLS[0])
         val imageStream = url.openStream()
         val expected = BitmapFactory.decodeStream(imageStream)
         imageStream.close()
@@ -98,7 +97,7 @@ class DronePhotosTest {
                 0,
                 true,
                 0,
-                CORRECT_URL_1
+                CORRECT_URLS[0]
             )
         )
         // For some reason the downloaded image "photo1" from the exact same link which he downloads the photo is not the same
@@ -132,7 +131,7 @@ class DronePhotosTest {
 
         val counter = CountDownLatch(1)
 
-        val url = URL(CORRECT_URL_1)
+        val url = URL(CORRECT_URLS[0])
         val imageStream = url.openStream()
         val expected = BitmapFactory.decodeStream(imageStream)
         imageStream.close()
@@ -145,7 +144,7 @@ class DronePhotosTest {
                 0,
                 true,
                 0,
-                CORRECT_URL_1
+                CORRECT_URLS[0]
             )
         )
         captureInfoPublisher.onNext(
@@ -180,16 +179,16 @@ class DronePhotosTest {
         val droneService = mock(DroneService::class.java)
         `when`(droneService.provideDrone()).thenReturn(DroneInstanceMock.droneSystem)
 
-        val info1 = CameraProto.CaptureInfo.newBuilder().setFileUrl(CORRECT_URL_1).build()
-        val info2 = CameraProto.CaptureInfo.newBuilder().setFileUrl(CORRECT_URL_2).build()
-        val list = listOf(info1, info2)
-        setPhotosList(list)
+        val captureInfos = CORRECT_URLS.map { url ->
+            CameraProto.CaptureInfo.newBuilder().setFileUrl(url).build()
+        }
+        setPhotosList(captureInfos)
 
         val photos: DronePhotos = DronePhotosImpl(droneService)
         val mutex = Semaphore(0)
 
         val expected: MutableList<Bitmap> = mutableListOf()
-        for (url in list.map { it.fileUrl }) {
+        for (url in CORRECT_URLS) {
             val imageStream = URL(url).openStream()
             expected.add(BitmapFactory.decodeStream(imageStream))
             imageStream.close()
