8819629b4d1c6a93e69e0f0d034a0aa05e6b68b3
Gabriel Fleischer <gabriel.fleischer@epfl.ch>
2021-05-25 22:21:27 +0200

    Add MissionPicturesActivity logic

--- app/src/main/java/ch/epfl/sdp/drone3d/ui/mission/MissionPicturesActivity.kt
+++ app/src/main/java/ch/epfl/sdp/drone3d/ui/mission/MissionPicturesActivity.kt
@@ -1,16 +1,32 @@
 package ch.epfl.sdp.drone3d.ui.mission
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
 import ch.epfl.sdp.drone3d.R
+import ch.epfl.sdp.drone3d.service.api.drone.DronePhotos
+import ch.epfl.sdp.drone3d.ui.ToastHandler
+import dagger.hilt.android.AndroidEntryPoint
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
+import timber.log.Timber
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MissionPicturesActivity : AppCompatActivity() {
+
+    companion object {
+        private const val SAMPLE_SIZE = 20
+    }
+
+    private var disposable: Disposable? = null
+
+    @Inject lateinit var photos: DronePhotos
     
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mission_pictures)
-
 
         //Create a "back button" in the action bar up
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
@@ -18,5 +34,26 @@ class MissionPicturesActivity : AppCompatActivity() {
         val recyclerView = findViewById<RecyclerView>(R.id.mission_pictures_view)
         val adapter = PictureViewAdapter()
         recyclerView.adapter = adapter
+
+        disposable = photos.getRandomPhotos(SAMPLE_SIZE)
+                .subscribeOn(Schedulers.io())
+                .retry(10 )
+                .subscribe(
+                        {
+                            adapter.submitList(it)
+                        },
+                        {
+                            ToastHandler.showToastAsync(this, R.string.error, Toast.LENGTH_SHORT, it.message)
+                            Timber.e(it, "Failed to download pictures")
+                            finish()
+                        }
+                )
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        disposable?.dispose()
+        disposable = null
     }
 }
